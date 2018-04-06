4d1ddf299715339bd42c8484569adc8f6d1efb94
Luan Vieira <pereiralv2010@gmail.com>
2018-04-05 21:49:29 -0300

    adicionando scroll

--- src/setupSocket/index.js
+++ src/setupSocket/index.js
@@ -2,7 +2,7 @@ import * as types from '../constants/ActionTypes'
 import { addUser, messageReceived, populateUsersList } from '../actions'
 
 const setupSocket = (dispatch, username) => {
-    const socket = new WebSocket('ws://localhost:8989')
+    const socket = new WebSocket('ws://6542c876.ngrok.io')
 
     socket.onopen = () => {
         socket.send(JSON.stringify({
